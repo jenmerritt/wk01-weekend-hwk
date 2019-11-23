4165ebf6f6ab6bb817ee1dbbfc52a14a8165360c
Jen Merritt <jenjomez@gmail.com>
2019-11-23 13:50:30 +0000

    test 3 passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,7 +1,11 @@
-def pet_shop_name(name)
-  return @pet_shop[:name]
+def pet_shop_name(shop)
+  return shop[:name]
 end
 
-def total_cash(sum)
-  return @pet_shop[:admin][:total_cash]
+def total_cash(shop)
+  return shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(shop, amount_to_add)
+  shop[:admin][:total_cash] += amount_to_add
 end
