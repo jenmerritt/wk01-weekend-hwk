b94703829aa765bcec51ad853967cba7f07532d4
Jen Merritt <jenjomez@gmail.com>
2019-11-23 13:59:35 +0000

    function for test 5 added and test passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -6,6 +6,10 @@ def total_cash(shop)
   return shop[:admin][:total_cash]
 end
 
-def add_or_remove_cash(shop, amount_to_add)
-  shop[:admin][:total_cash] += amount_to_add
+def add_or_remove_cash(shop, amount_to_add_or_remove)
+  shop[:admin][:total_cash] += amount_to_add_or_remove
+end
+
+def pets_sold(shop)
+  return shop[:admin][:pets_sold]
 end
