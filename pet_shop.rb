8b062acf99e14c897b066e448139690444bbf560
Jen Merritt <jenjomez@gmail.com>
2019-11-24 18:58:07 +0000

    add comments

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,26 +1,40 @@
+# get shop name
+
 def pet_shop_name(shop)
   return shop[:name]
 end
+
+# get total cash in shop from admin hash
 
 def total_cash(shop)
   return shop[:admin][:total_cash]
 end
 
+# add or remove cash from shop total in admin hash
+
 def add_or_remove_cash(shop, amount_to_add_or_remove)
   shop[:admin][:total_cash] += amount_to_add_or_remove
 end
+
+# get number of pets sold by shop from admin hash
 
 def pets_sold(shop)
   return shop[:admin][:pets_sold]
 end
 
+# incraese the number of pets sold in admin hash
+
 def increase_pets_sold(shop, pets_sold)
   shop[:admin][:pets_sold] += pets_sold
 end
 
+# count items in pets array to take stock count
+
 def stock_count(shop)
   return shop[:pets].count
 end
+
+# find pets by breed and return array of names of pets found for that breed
 
 def pets_by_breed(shop, breed_to_find)
   pets_found = []
@@ -32,6 +46,8 @@ def pets_by_breed(shop, breed_to_find)
   return pets_found
 end
 
+# find pets by name and return hash of pets found
+
 def find_pet_by_name(shop, name_to_find)
   for pet in shop[:pets]
     if pet[:name] == name_to_find
@@ -41,6 +57,8 @@ def find_pet_by_name(shop, name_to_find)
   return pet_name
 end
 
+# remove a specific pet from the pets array by name
+
 def remove_pet_by_name(shop, name_to_remove)
   for pet in shop[:pets]
     if pet[:name] == name_to_remove
@@ -49,27 +67,38 @@ def remove_pet_by_name(shop, name_to_remove)
   end
 end
 
+# add a new pet (hash) as new item in pets array
+
 def add_pet_to_stock(shop, new_pet)
   shop[:pets] << new_pet
 end
+
+# return how much cash a specific customer has
 
 def customer_cash(customer)
   return customer[:cash]
 end
 
+# reduce cash of a specific customer by given amount
+
 def remove_customer_cash(customer, cash_to_remove)
   customer[:cash] -= cash_to_remove
 end
+
+# return number of pets a specific customer has
 
 def customer_pet_count(customer)
   customer[:pets].count
 end
 
+# add a new pet to a customers pets array
+
 def add_pet_to_customer(customer, new_pet)
   customer[:pets] << new_pet
 end
 
-#optional
+# optional
+# check customer cash amount against price of pet to see if customer can afford pet, return true/false.
 
 def customer_can_afford_pet(customer, pet)
   if customer[:cash] >= pet[:price]
